994c3e472d90cbc6ce16eb89fcc505a499395726
Chacha <anne-charlotte.jeanvoine@laposte.net>
2022-02-02 11:38:29 +0100

    fusion des deux parties hotline, travaillé par florent et charlotte

--- View/searchHOT.php
+++ View/searchHOT.php
@@ -4,66 +4,102 @@
         include "./modules/ClassDosHot.php";
         $tech = new TechHOTLINE();
         $varFolder = $tech->getFolderNum();
+        $tech->searchDos();
+     
     ?>
     <section class="floSection">
         <form action="index.php" method="POST">
             <fieldset class="floFieldsetprincipal">
                 <legend class="floLegend">Création de dossier : </legend>
-                <fieldset class="floFieldsetInfo">
-                    <legend class="floLegend">Informations du dossier : </legend>   
-                    <label class="floLabel" class="floLabel" for="nom">Numéro de dossier : </label>
-                    <input type="text" name="nom" id="NumDossier" placeholder="<?php echo $varFolder; ?>" readonly><br>
+                <fieldset class="floFieldsetDos">
+                    <legend class="floLegend">Informations du dossier :</legend>  
+                    <label class="floLabel" for="nomDos">Numéro de dossier : </label>
+                    <input type="text" name="numDos" id="NumDossier" placeholder="<?php echo $varFolder; ?>" readonly><br>
+                    <label class="flolabel" for="etat">Etat du dossier :</label>
+                    <input type="text" name="etat" placeholder="En attente de récéption" readonly><br>
+                    <input type="hidden" name="etatDos" value="1">
                     <label class="floLabel" for="maDate">Date &amp; Heure :</label>
                     <input type="text" name="maDate" id="maDate" value="<?php echo date('d-m-Y') . " & " . date('h:i:s') ; ?>" readonly><br>
-                    <label class="floLabel" for="Objet">Objet de l'appel : </label>
-                    <select id="Objet">
-                        <option>Réparation</option>
-                        <option>Demande d'échange</option>
-                        <option>Réclamation</option>
-                        <option>Autre</option>
-                    </select>
+                    <!-- <label class="floLabel" for="Objet">Objet de l'appel : </label>
+                    <select name="search-type">
+                                 <option>Selectionner</option>
+                                 <option value="1">En attente de recep.</option>
+                                 <option value="2">Reçu</option>
+                                 <option value="3">Attente de Diagnostic</option>
+                                 <option value="4">Attente de cloture</option>
+                                 <option value="5">Cloturé</option>
+                                 <option value="6">Annulé</option>
+                         </select> -->
                 </fieldset>
             
-                <fieldset class="floFieldsetCoo">
+                <fieldset class="floFieldsetDos">
+                    <?php
+                        // $tech->showDossier($tech->searchHot());
+                    ?>
+
                     <legend class="floLegend">Coordonnées client : </legend>
+                    <label class="floLabel" for="nom">Nom : </label>
+                    <input type="text" name="nom" id="nom" placeholder="<?php
+                         if(isset($_POST['query'])) {
+                            $search = new TechHOTLINE();
+                            $tableau = $search->searchDog($_POST['query']);    
+                            echo $tableau[0]['ADR_DENOMINATION'];   
+                        }
+                    ?>" readonly><br>
                     <label class="floLabel" for="NumCommande">Numéro de commande : </label>
+                    <input type="text" name="numCom" id="NumCommande" placeholder="<?php
+                        if(isset($_POST['query'])) {
+                            $search = new TechHOTLINE();
+                            $tableau = $search->searchDog($_POST['query']);    
+                            echo $tableau[0]['OHR_ORDERNUMBER'];   
+                        }
+                    ?>" readonly><br>
+                    <input type="hidden" name="idCom" id="idCom" value="<?php
+                        if(isset($_POST['query'])) {
+                            $search = new TechHOTLINE();
+                            $tableau = $search->searchDog($_POST['query']);    
+                            echo $tableau[0]['OHR_ID'];   
+                        }
+                    ?>" readonly><br>
+
 <!-- Partie code par Florent le 26/01/2022 -->
-                    <select name="NumCom" onchange=""><br>
-                    <option value="" >Sélectionner</option>
+                    <!-- <select name="NumCom" onchange=""><br>
+                    <option value="" >Sélectionner</option> -->
                         <?php
-                        $tech->getOrderNum(); 
+                        // $tech->getOrderNum(); 
                         ?>
-                    <label class="floLabel" for="maCiv">Civilité :</label>
+                    <!-- <label class="floLabel" for="maCiv">Civilité :</label>
                     <input type="radio" name="civ" id="mr" value="0">
                     <label class="floLabel" for="maCiv">Mr</label>
                     <input type="radio" name="civ" id="mme" value="1">
                     <label class="floLabel" for="mme">Mme</label>
-                    <br>
-                    <label class="floLabel" for="nom">Nom : </label>
-                    <input type="text" name="nom" id="nom" >
                     <label class="floLabel" for="prenom">Prénom : </label>
                     <input type="text" name="prenom" id="prenom"><br>
                     <label class="floLabel" for="email">Email : </label>
-                    <input type="email" name="email" id="email" placeholder="exemple@exemple.fr">
+                    <input type="email" name="email" id="email" placeholder="exemple@exemple.fr"> -->
+
                 </fieldset>
-                <fieldset class="floFieldsetPro">
+                <fieldset class="floFieldsetDos">
                     <legend class="floLegend">Produit : </legend>
-                    <label class="floLabel" for="nom">Nom : </label>
-                    <input type="text" name="tdsid" id="NomProduit" placeholder="TDS_ID"><br>
-                    <label class="floLabel" for="nom">Type : </label>
-                    <input type="text" name="prdid" id="TypeProduit" placeholder="PRD_ID"><br>
-                    <label class="floLabel" for="nom">Référence : </label>
-                    <input type="number" name="orhid" id="RefProduit" placeholder="ORH_ID">
-                    <!-- <input type="number" name="" id="PrixProduit" placeholder="Prix"> -->
+                    <!-- <label class="floLabel" for="nom">Nom : </label> -->
+                    <!-- <input type="text" name="tdsid" id="NomProduit" placeholder="TDS_ID"><br> -->
+                    <?php 
+                     if(isset($_POST['query'])) {
+                        $search = new TechHOTLINE();
+                        // var_dump('query');
+                        $search->searchHot($_POST['query']);            
+                        }
+                    ?>
                     <br>
-                    <label class="floLabel">Description : </label><br>
-                    <textarea name="comdiag" id="" cols="70" rows="10" placeholder="Détail de l'appel du client"></textarea>
+                    <label class="floLabel">Diagnostic : </label><br>
+                    <textarea name="comDiag" id="" cols="70" rows="10" placeholder="Détail de l'appel du client"></textarea>
                 </fieldset>
+
                 <div>
                     <input class="floButton" type="reset" value="Remise à zéro" />
-                    <button class="floButton" type="submit">Envoyer le formulaire</button>
+                    <button class="floButton" type="submit">Créer le dossier</button>
                         <?php 
-                            if(isset($_POST['civ']) && isset($_POST['tdsid']) && isset($_POST      ['prdid']) && isset($_POST["orhid"]) && isset($_POST['comdiag'])) {
+                            if(isset($_POST['etatDos']) && isset($_POST['produit']) && isset($_POST['idCom']) && isset($_POST['comDiag'])) {
                             $hot = new TechHOTLINE();
                             $hot->postForm($_POST);
                             }
--- modules/ClassDosHot.php
+++ modules/ClassDosHot.php
@@ -2,6 +2,26 @@
 
 Class TechHOTLINE {
 
+    // PArtie code par Florent le 01/02/2022
+    // Fonction d'affichage des résultats de la recherche d'un dossier SAV
+    public function showProduct($results){   
+            echo '<table>';
+            echo '<tr>';
+            echo '<th></th><th>Nom</th><th>Référence</th><th>Type</th>';
+            echo '</tr>';
+        foreach($results as $data){
+            echo '<tr>';
+            echo '<th><input type="radio" name="produit" value="'.$data['ODT_ID'].'"></th>';
+            echo '<td>' .$data['PRD_DESCRIPTION'].'</td>';
+            echo '<td>' .$data['PRD_REFERENCE'].'</td>';
+            echo '<td>' .$data['PRD_DESIGNATION'].'</td>';
+            echo '</tr>';
+        }
+            echo '</table>';
+
+    }
+
+
     // Fonction qui affiche le numéro de dossier automatiquement
     public function getFolderNum(){
         $db = Connector::getInstance();
@@ -11,65 +31,77 @@ Class TechHOTLINE {
         // var_dump($varFolder);
         $varFolder = intval($varFolder['DSV_ID']) ;
         // var_dump($varFolder); // pour tester
-        return $varFolder = ($varFolder + 1);
+        $varFolder = ($varFolder + 1);     
+        return $varFolder;   
+    }
+
+        // PArtie code par Florent le 01/02/2022
+    // Fonction affichage de la recherche par numéro de téléphone
+    public function searchDos() {       
+        echo '<form action="" method="POST">';
+        echo '<fieldset class="floFieldsetInfo">';
+        echo '<legend class="floLegend">Recherche d\'un dossier : </legend>';
+        echo '<label class="floLabel" for="nom">Numéro de téléphone : </label>';
+        echo '<input type="text" name="query" required>';
+        echo '<input class="floButton" type="submit" value="Recherche"/>';
+        echo '</fieldset>';
+        echo '</form><br>';
+    }
+
+       // Partie code par Florent le 31/01/2022
+    // Fonction de recherche par numéro de téléphone pour afficher les produits.
+    public function searchHot($query){
+        $db = Connector::getInstance();
+        $QuerySearch = $db->prepareQuery("SELECT `ODT_ID`, `PRD_REFERENCE`, `PRD_DESIGNATION`, `PRD_DESCRIPTION` FROM `t_d_orderheader_ohr` AS OHR
+        JOIN `t_d_address_adr` AS ADR ON OHR.OHR_ADR_ID_PAYMENT = ADR.ADR_ID
+        JOIN `t_d_orderdetail_odt` AS ODT ON ODT.ODT_OHR_ID = OHR.OHR_ID
+        JOIN `t_d_product_prd` AS PRD ON ODT.ODT_PRD_ID = PRD.PRD_ID
+        WHERE `ohr_deliveryphone` LIKE ? ");    
+        $QuerySearch -> bindvalue( 1 , $query."%", PDO::PARAM_STR);      
+        $QuerySearch->execute();
+        $results = $QuerySearch->fetchAll();
+        // var_dump($results); // pour tester
+        return $this->showProduct($results);
+    }
+
+    // PArtie code par Sofiane le 01/02/2022
+    // Fonction de recherche des résultats par rapport au numéro de téléphone
+    public function searchDog($query){
+        $db = Connector::getInstance();
+        $QuerySearch = $db->prepareQuery("SELECT `OHR_DELIVERYPHONE`, `OHR_ID`, `OHR_ORDERNUMBER`, `OHR_DATE` , `ADR_DENOMINATION` FROM `t_d_orderheader_ohr` AS OHR
+        JOIN `t_d_address_adr` AS ADR ON OHR.OHR_ADR_ID_PAYMENT = ADR.ADR_ID
+        JOIN `t_d_orderdetail_odt` AS ODT ON ODT.ODT_OHR_ID = OHR.OHR_ID
+        JOIN `t_d_product_prd` AS PRD ON ODT.ODT_PRD_ID = PRD.PRD_ID
+        WHERE `ohr_deliveryphone` LIKE ? ");    
+        $QuerySearch -> bindvalue( 1 , $query."%", PDO::PARAM_STR);      
+        $QuerySearch->execute();
+        $results = $QuerySearch->fetchAll();
+        return $results;
     }
 
     // Fonction qui recherche la liste de tous les numéros de commande
-    public function getOrderNum(){
-        $db = Connector::getInstance();
-        $QueryNumOrder = $db->prepareQuery('SELECT `OHR_ORDERNUMBER` FROM `t_d_orderheader_ohr` order by `ohr_ordernumber` DESC;');        
-        $QueryNumOrder->execute();
-        $result = $QueryNumOrder->fetchAll();
-        return $this->createOptions($result);
-    }
-
-    // Fonction qui créé les options
-    public function createOptions($param){
-        foreach($param as $key) {
-            foreach($key as $value => $entries) :
-                echo '<option value="'.$entries.'">'.$entries.'</option>';
-            endforeach;
-        }
-    }
-
-    // Fonction qui enregistre les données du formulaire dans la BDD
-    public function postForm($valuescoo) {
-                
-        $db = Connector::getInstance();
-        $stmt = $db->prepareQuery("INSERT INTO t_d_dossier_sav_dsv (DSV_ETAT, DSV_TDS_ID, DSV_PRD_ID, DSV_ORH_ID, DSV_COM_DIAG_INITIAL) VALUES (? , ? , ? , ? , ? )");
-        
-        $stmt -> bindvalue( 1 , $valuescoo['civ'], PDO::PARAM_INT);
-        $stmt -> bindvalue( 2 , $valuescoo['tdsid'], PDO::PARAM_INT);
-        $stmt -> bindvalue( 3 , $valuescoo['prdid'], PDO::PARAM_INT);
-        $stmt -> bindvalue( 4 , $valuescoo['orhid'], PDO::PARAM_INT);
-        $stmt -> bindvalue( 5 , $valuescoo['comdiag'], PDO::PARAM_STR);
-        
-        $result = $stmt->execute();
-        // return var_dump($stmt); // Pour tester
-     
-    }
-    
-    // Remplissage des champs automatiques
-    // public function readinfo() {
-    //     if (isset($_GET["value1"])) {
-    //         $NumCommande = $_GET["value1"];
-    //         echo $_GET["value1"].$NumCommande;
-    //         var_dump($NumCommande);
-    //     }
-        
-    //     if(isset($_POST['value1'])){
-    //         $entry = $_POST['value1'];
-    //         $db = Connector::getInstance();
-    //         $QueryNumOrder = $db->prepare('SELECT `adr_denomination`, `adr_city` FROM `t_d_address_adr`
-    //         join `t_d_orderheader_ohr` on `t_d_address_adr`.`ADR_ID` = `t_d_orderheader_ohr`.`OHR_ADR_ID_PAYMENT` 
-    //         WHERE `OHR_ORDERNUMBER` = ?'); 
-    //         $QueryNumOrder->bindValue(1, $entry, PDO::PARAM_STR); 
-    //         $QueryNumOrder->execute();
-    //         $varadr = $QueryNumOrder->fetchAll();
-    //         var_dump($varadr);
-    //         }
+    // public function getOrderNum(){
+    //     $db = Connector::getInstance();
+    //     $QueryNumOrder = $db->prepareQuery('SELECT `OHR_ORDERNUMBER` FROM `t_d_orderheader_ohr` order by `ohr_ordernumber` DESC;');        
+    //     $QueryNumOrder->execute();
+    //     $result = $QueryNumOrder->fetchAll();
+    //     return $this->createOptions($result);
     // }
 
+
+    // Fonction qui enregistre les données du formulaire dans la BDD
+    public function postForm($valuescoo) {            
+        $db = Connector::getInstance();
+        $stmt = $db->prepareQuery("INSERT INTO t_d_dossier_sav_dsv (DSV_TDS_ID, DSV_PRD_ID, DSV_ORH_ID, DSV_COM_DIAG_INITIAL) VALUES (? , ? , ? , ?)");
+        
+        $stmt -> bindvalue( 1 , $valuescoo['etatDos'], PDO::PARAM_INT);
+        $stmt -> bindvalue( 2 , $valuescoo['produit'], PDO::PARAM_INT);
+        $stmt -> bindvalue( 3 , $valuescoo['idCom'], PDO::PARAM_INT);
+        $stmt -> bindvalue( 4 , $valuescoo['comDiag'], PDO::PARAM_STR);
+        
+        $result = $stmt->execute();
+        return var_dump($result); // Pour tester
+    }
 }
 
 ?>
